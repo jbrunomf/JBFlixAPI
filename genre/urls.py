eaac57bf6e7ce0986fadac3f7e4068d661c5a284
João Bruno <jbrunomf@outlook.com>
2025-03-09 21:55:11 -0400

    Add API endpoint for listing and creating genres
    Replaced function-based view with a class-based ListCreateAPIView for genres. Added a serializer for the Genre model and updated URLs to include the genre API endpoint. This introduces API support for retrieving and adding genre data.

--- genre/urls.py
+++ genre/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from genre.views import GenreCreateListView
+from genre.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('', GenreCreateListView.as_view(), name='genre_list'),
+    path('/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre_detail_view'),
 ]
--- genre/views.py
+++ genre/views.py
@@ -7,3 +7,7 @@ from genre.serializers import GenreSerializer
 class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
